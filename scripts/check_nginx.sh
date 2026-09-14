75046a52da9ce5ef33bcbae06ba72f73e40999da
Mithul <mithul08xyz@gmail.com>
2026-09-14 23:04:01 +0530

    Update check_nginx.sh

--- scripts/check_nginx.sh
+++ scripts/check_nginx.sh
@@ -1,1 +1,15 @@
 
+if systemctl is-active --quiet nginx
+then
+    echo "Nginx is running"
+else
+    echo "Nginx is DOWN"
+    echo "Attempting to restart nginx..."
+    sudo systemctl restart nginx
+    if systemctl is-active --quiet nginx
+    then
+        echo "Nginx restarted successfully"
+    else
+        echo "ERROR:Nginx failed to restart"
+    fi
+fi
